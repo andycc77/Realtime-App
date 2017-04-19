dd93757b3e059da13b2d68789a7be9573cb4a4d7
Allen <andycc77e@gmail.com>
2017-04-19 17:20:53 +0800

    socket.in即時聊天室

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 app.get('/', function (request,response) {
     response.sendFile(__dirname+'/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat.message',function (message) {
+        io.emit('chat.message',message);
+    });
 });
 
 http.listen(3000,function () {
